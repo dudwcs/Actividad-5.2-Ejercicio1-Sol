d593fa27710ddd799142c5d484b77f74ce4c3e34
dwcs-2324 <dwcs2324@gmail.com>
2024-01-15 17:45:12 +0100

    A5.2 ej 1

--- visitas.php
+++ visitas.php
@@ -11,14 +11,29 @@
     <h1>Visits counter</h1>
 
     <?php
+
+
     if (isset($_COOKIE["visitas"])) {
+
+        if (isset($_POST["borrar"])) {
+            //Para borrar explícitamente no servidor e marcar como deleted:
+            //2º argumento debe ser cadea baleira ou false
+            setcookie("visitas", '', time() - 3600);            
+            unset($_COOKIE["visitas"]);
+        }
+    
 
         $visitas = $_COOKIE["visitas"];
         setcookie("visitas", ++$visitas, time() + 3600);
         //El nuevo valor no estará disponible en $_COOKIE hasta la próxima petición HTTP 
         //Por ese motivo no podemos usar $_COOKIE["visitas] para mostrar su valor
-
     ?>
+        <form method="post">
+            <!-- Submit button -->
+            <input type="submit" name="borrar" class="btn btn-secondary btn-block mb-4" value="Borrar cookies"></button>
+        </form>
+
+
 
         <p>Nos ha visitado <?= $visitas ?> veces</p>
     <?php  } else {
@@ -27,7 +42,11 @@
         <p>Bienvenido</p>
     <?php
     }
+
+
     ?>
+
+
 
 </body>
 
